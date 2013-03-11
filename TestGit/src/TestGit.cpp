bced59af00e2735b17bfb4f4285440b4e1d2b5cf
blal <blal.bachir@gmail.com>
2013-03-11 21:03:12 +0100

    commit init3

--- TestGit/src/TestGit.cpp
+++ TestGit/src/TestGit.cpp
@@ -14,5 +14,7 @@ int main() {
 	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
 	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
 	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
 	return 0;
 }
